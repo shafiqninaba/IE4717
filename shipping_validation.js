2faf394b58c092281fc2bf12d0cb03c9a7e7dc64
Shafiq Ninaba <79973831+shafiqninaba@users.noreply.github.com>
2023-10-04 01:14:23 +0800

    Added pages involved in cart flow

---
+++ shipping_validation.js
@@ -0,0 +1,22 @@
+function validateForm() {
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var email = document.getElementById('email').value;
+    var mobile = document.getElementById('mobile').value;
+    var address = document.getElementById('address').value;
+
+    // Email validation using a regular expression
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!email.match(emailRegex)) {
+        alert('Please enter a valid email address');
+        return false;
+    }
+
+    var mobileRegex = /^(\+\d*)$/;
+    if (!mobile.match(mobileRegex)) {
+        alert('Please include your country code in the mobile number');
+        return false;
+    }
+
+    return true;
+}
