c586faf910fd506af081cade4b1848f5f485739c
Shafiq Ninaba <79973831+shafiqninaba@users.noreply.github.com>
2023-11-06 15:19:58 +0800

    cart logic
    added functionality to cart checkout

--- cart.php
+++ cart.php
@@ -9,6 +9,74 @@ function header_class($function){
   if ($function) {echo 'hidden';} else {echo '';}
 }
 
+// function to get shopping cart items
+function get_shopping_cart(){
+
+    include "dbconnect.php";
+    $user_id = $_SESSION['user_id'];
+    $query = "SELECT shopping_cart_item.id,product_info.id AS product_item_id,product_info.name,product_info.price,product_info.product_image,shopping_cart_item.size,shopping_cart_item.qty FROM shopping_cart_item INNER JOIN product_info ON shopping_cart_item.product_item_id=product_info.id WHERE shopping_cart_item.user_id =".$user_id;
+    $result = $dbcnx->query($query);
+    $cart = array();
+    while ($row = $result->fetch_assoc()){
+        $cart[] = $row;
+    }
+    return $cart;
+
+}
+// function to delete item from shopping cart
+function delete_shopping_cart_item($product_item_id){
+
+    include "dbconnect.php";
+    $query = "DELETE FROM shopping_cart_item WHERE id =".$product_item_id;
+    $result = $dbcnx->query($query);
+}
+if(isset($_POST['remove_button'])) { 
+    delete_shopping_cart_item($_POST['remove_button']);
+} 
+
+// function to checkout items in shopping cart
+function checkout_items($cart_items,$user_id,$order_total){
+        include "dbconnect.php";
+        // insert data into site_user table
+        $sql = "INSERT INTO shop_order (user_id,order_total) VALUES (?, ?)";
+        $stmt = $dbcnx->prepare($sql);
+        $stmt->bind_param('id', $user_id,$order_total);
+        $stmt->execute();
+    
+        // get the user id of the newly inserted row
+        $shop_order_id = $stmt->insert_id;
+        $stmt->free_result();
+        
+        // insert $cart_items data into order_line table
+        foreach ($cart_items as $index=>$item) {
+            $product_item_id = $item['product_item_id'];
+            $qty = $item['qty'];
+            $price = $item['price'];
+            $sql = "INSERT INTO order_line (product_item_id,order_id,qty,price) VALUES (?,?,?,?)";
+            $stmt = $dbcnx->prepare($sql);
+            $stmt->bind_param('iiid', $product_item_id,$shop_order_id,$qty,$price);
+            $stmt->execute();
+            $stmt->free_result();
+        }      
+        // remove items from shopping cart after checking out
+        foreach ($cart_items as $index=>$item) {
+            delete_shopping_cart_item($item['id']);
+        }
+header("Location: payment.php");
+
+}
+
+// function to edit qty and size of item in shopping cart
+function edit_shopping_cart_item($product_item_id,$qty,$size){
+    include "dbconnect.php";
+    $query = "UPDATE shopping_cart_item SET qty =".$qty.", size =".$size." WHERE id =".$product_item_id;
+    $result = $dbcnx->query($query);
+}
+$cart_items = get_shopping_cart();
+$_SESSION['cart_items'] = $cart_items;
+if(isset($_POST['checkout-button'])) { 
+    checkout_items($cart_items, $_SESSION['user_id'], $_POST['checkout-button']);
+} 
 ?>
 
 <!DOCTYPE html>
@@ -48,58 +116,38 @@ function header_class($function){
         </script>
     </div>
     </div>
-
+<?php if (logged_in() && count($cart_items)>0){
+    
+    ?>
 <div class="cart_body_content">
     <div class="cart_items">
         <h1>Your cart</h1>
         <p>Not ready to checkout? <a href="shop.php">Continue shopping</a></p>
-        <div class="cart_item">
-            <div class="cart_item_image">
-                <img src="images/placeholder.png" alt="placeholder">
-            </div>
-            <div class="cart_item_details">
-                <h3>Product Name</h3>
-                <p>Size: US10</p>
-                <p>Quantity: 1</p>
-                <p>Price: $100</p>
-            </div>
-            <div class="cart_item_remove">
-                <button>X</button>
-            </div>
-            </div>
-        <hr>
-        <div class="cart_item">
-            <div class="cart_item_image">
-                <img src="images/placeholder.png" alt="placeholder">
-            </div>
-            <div class="cart_item_details">
-                <h3>Product Name</h3>
-                <p>Size: US10</p>
-                <p>Quantity: 1</p>
-                <p>Price: $100</p>
-            </div>
-            <div class="cart_item_remove">
-                <button>X</button>
-            </div>
+    <?php
+        foreach ($cart_items as $index=>$item) {
+            ?>
+            <div class="cart_item">
+                <div class="cart_item_image">
+                    <img src="<?php echo $item['product_image'] ?>" alt="Image for <?php echo $item['name'] ?>">
+                </div>
+                <div class="cart_item_details">
+                    <h3><?php echo $item['name'] ?></h3>
+                    <p>Size: <?php echo 'US'.$item['size'] ?></p>
+                    <p>Quantity: <?php echo $item['qty'] ?></p>
+                    <p>Price: $<?php echo $item['price'] ?></p>
+                </div>
+                <div class="cart_item_remove">
+                    <form method="POST">
+                    <button title="Remove item" name="remove_button" value="<?php echo $item['id'] ?>" onclick="return confirm('Are you sure you want to remove this item?')">X</button>                    </form>
+                
+                </div>
         </div>
         <hr>
-        <div class="cart_item">
-            <div class="cart_item_image">
-                <img src="images/placeholder.png" alt="placeholder">
-            </div>
-            <div class="cart_item_details">
-                <h3>Product Name</h3>
-                <p>Size: US10</p>
-                <p>Quantity: 1</p>
-                <p>Price: $100</p>
-            </div>
-            <div class="cart_item_remove">
-                <button>X</button>
-            </div>
-        </div>
-        
-    </div>
-<div class="order_summary">
+        <?php
+}
+?>
+</div>
+<div class="order_summary fixed">
     <h2>Order Summary</h2>
     <div>
         <table>
@@ -110,30 +158,25 @@ function header_class($function){
                 <td>Size</td>
                 <td>Price</td>
             </tr>
+            <?php
+        $cart_items = get_shopping_cart();
+        $total_price = 0;
+        foreach ($cart_items as $index=>$item) {
+            ?>
             <tr>
-                <td>1.</td>
-                <td>Product here</td>
-                <td>1</td>
-                <td>US10</td>
-                <td>$100</td>
+                <td><?php echo $index+1?></td>
+                <td><?php echo $item['name'] ?></td>
+                <td><?php echo $item['qty'] ?></td>
+                <td><?php echo 'US'.$item['size'] ?></td>
+                <td>$<?php echo $item['qty']*$item['price'] ?></td>
             </tr>
-            <tr>
-                <td>2.</td>
-                <td>Product here</td>
-                <td>1</td>
-                <td>US10</td>
-                <td>$100</td>
-            </tr>
-            <tr>
-                <td>3.</td>
-                <td>Product here</td>
-                <td>1</td>
-                <td>US10</td>
-                <td>$100</td>
-            </tr>
+            <?php
+            $total_price += $item['qty']*$item['price'];
+}
+?>
             <tr id="subtotal">
                 <td colspan="4">Subtotal</td>
-                <td>$300</td>
+                <td>$<?php echo $total_price+3;?></td>
             </tr>
             <tr>
                 <td colspan="4">Shipping</td>
@@ -142,16 +185,40 @@ function header_class($function){
             <tr><td colspan="5"><hr></td></tr>
             <tr>
                 <td colspan="4">Total</td>
-                <td>$303</td>
+                <td>$<?php echo $total_price+3;?></td>
             </tr>
         </table>
-        <a href="shipping.php"><button id="checkout-button">Checkout</button></a>
+        <form method="POST">
+        <button name="checkout-button" id="checkout-button" value="<?php echo $total_price+3;?>">Checkout</button>
+</form>
     </div>
-<div class="checkout">
 </div>
+</div>
+<?php }
+elseif (logged_in() && count($cart_items)==0){?>
+
+    <div class="cart_body_content">
+    <div class="cart_items">
+        <h1>Your cart</h1>
+
+        <p>Your cart is empty. <a href="shop.php">Continue browsing</a> to add items to your cart.</p>
 </div>
 </div>
 
+<?php }
+    
+
+else{?>
+<div class="cart_body_content">
+    <div class="cart_items">
+        <h1>Your cart</h1>
+
+        <p>Please <a href="login.php">login</a> to view your cart. Or <a href="shop.php">Continue browsing</a></p>
+</div>
+</div>
+
+
+<?php }?>
     <footer>
     <div class = "footer">
       <div class="newsletter footer-left"  >
