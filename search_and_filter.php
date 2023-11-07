1ad66c1401a6c78f0c25bbc700056151e88ae507
Shafiq Ninaba <79973831+shafiqninaba@users.noreply.github.com>
2023-11-07 22:41:57 +0800

    search_and_filter.php hotfix

--- search_and_filter.php
+++ search_and_filter.php
@@ -6,10 +6,10 @@ if (isset($_GET['search'])) {
     $_SESSION['search_result'] = ($_GET['search']);
 } 
 //category filter from GET request
+
 if (isset($_GET['category_filter'])) {
-    
     $_SESSION['category_filter'] = ($_GET['category_filter']);
-    
+
     $category_filter_query = "";
     foreach($_SESSION['category_filter'] as $category){
         $category_filter_query .= "'$category'";
@@ -19,6 +19,9 @@ if (isset($_GET['category_filter'])) {
         }
     }
     $_SESSION['category_filter_query'] = $category_filter_query;
+}
+else{
+    unset($_SESSION['category_filter']);
 }
 
 header("Location: shop.php");
